15dd6ab96fd71eec8a2bb3bbad2e68737621d129
Sukincen <33780347+Sukincen@users.noreply.github.com>
2018-01-21 09:31:31 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -670,7 +670,7 @@ extension["虚空虫翼"]={
 };
 extension["弹丸杀"]={
     author:"fux2",
-    intro:"武将包内包含游戏《超级弹丸论破》系列作品的登场人物，这是用爱制作的武将包，所以不要在意什么平衡性啦，持续更新中",
+    intro:"武将包内包含游戏《超级弹丸论破》系列作品的登场人物，这是用爱制作的武将包，所以不要在意什么平衡性啦，持续更新中（新增音无凉子和战刃骸20180121）",
     size:"2.1MB",
     netdisk:"https://pan.baidu.com/s/1mhXRd9m",
     forum:"https://tieba.baidu.com/p/4802167753",

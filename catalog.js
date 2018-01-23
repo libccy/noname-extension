1d6dcd6251002080d9cbb27235c09665f940694b
Sukincen <33780347+Sukincen@users.noreply.github.com>
2018-01-23 16:44:55 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -572,7 +572,7 @@ extension["列女传"]={
 
 
 extension["新武将"]={
-	intro:"使用、更新前请先打开兼容模式！本扩展包含部分三国改版新武将和秦时明月、火影忍者等作品的人物，强度略高，基本上都有配音。",
+	intro:"下载、使用、更新前请先打开兼容模式！本扩展包含部分三国改版新武将和秦时明月、火影忍者等作品的人物，强度略高，基本上都有配音。",
 	author:"Sukincen",
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",

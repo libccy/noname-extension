c6af2f1dba10c3276a3c9922d6771b17ce7508ee
Sukincen <33780347+Sukincen@users.noreply.github.com>
2018-04-19 17:48:50 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -605,11 +605,11 @@ extension["列女传"]={
 
 
 extension["新武将"]={
-	intro:"下载、使用、更新前请先打开兼容模式！本扩展包含部分三国改版新武将和秦时明月、火影忍者等作品的人物，强度略高，基本上都有配音。（此次更新：完善武将“鬼灯幻月” ；新增武将“真禅圣王”,更新日期：2018-04-12",
+	intro:"下载、使用、更新前请先打开兼容模式！本扩展包含部分三国改版新武将和秦时明月、火影忍者等作品的人物，强度略高，基本上都有配音。（此次更新：新增武将“君麻吕”,更新日期：2018-04-19",
 	author:"Sukincen",
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",
-	version:"20170412",
+	version:"20170419",
 	size:"37MB",
     version:4,
     files:

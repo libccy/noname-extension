bb9bb2820350e7a3e31a9397b9fb8f993d442248
Sukincen <33780347+Sukincen@users.noreply.github.com>
2018-06-27 09:09:57 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -522,13 +522,13 @@ extension["列女传"]={
 
 
 extension["新武将"]={
-	intro:"本扩展包含部分三国新将和火影忍者、秦时明月、武庚纪（封神纪）等作品的人物，强度略高，基本上都有配音，已增加联机功能！但联机版本要一致！建议打开兼容模式以提升对不同扩展的兼容性！更新日期：2018-06-25",
+	intro:"本扩展包含部分三国新将和火影忍者、秦时明月、武庚纪（封神纪）等作品的人物，强度略高，基本上都有配音，已增加联机功能！但联机版本要一致！（目前更新版本为2.2版，短期内不再更新）建议打开兼容模式！更新日期：2018-06-27",
 	author:"Sukincen",
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",
-	version:"2.1",
+	version:"2.2",
 	size:"48MB",
-    version:2.1,
+    version:2.2,
     files:
 
 ["xwj_xu_mianju.png","xwj_xu_shoulijian.png","xwj_xu_kuwu.png","xwj_xu_xuelunyang.png",

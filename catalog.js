52265baba9fd93c64b1908628116e076904e5b54
Sukincen <33780347+Sukincen@users.noreply.github.com>
2018-11-27 09:01:37 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -296,13 +296,13 @@ extension["列女传"]={
 
 
 extension["新武将"]={
-	intro:"本扩展包含部分三国新将、火影忍者、秦时明月、封神纪等作品的人物，强度略高但彼此平衡，可联机！因采用高清图片、高质配音，故体积略大，可自行删减素材。另外，请自行将配音文件xwj_xus_shoulijian和zbfs复制到audio-card-male/female这两个文件夹里（两处各一个）。更新：完善OL鲍三娘，修复天魁碎岳BUG。更新日期：2018-11-25",
+	intro:"本扩展包含部分三国新将、火影忍者、秦时明月、封神纪等作品的人物，强度略高但彼此平衡，可联机！因采用高清图片、高质配音，故体积略大，可自行删减素材。另外，请自行将配音文件xwj_xus_shoulijian和zbfs复制到audio-card-male/female这两个文件夹里（两处各一个）。更新：修复天魁碎岳BUG，开新坑“魔法少女小园萌战杀系列”。更新日期：2018-11-27",
 	author:"Sukincen",
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",
-	version:"2.39",
+	version:"2.40",
 	size:"66MB",
-    version:2.39,
+    version:2.40,
     files:
 
 ["xwj_xus_mianju.png","xwj_xus_shoulijian.png","xwj_xus_kuwu.png","xwj_xus_xuelunyang.png","xwj_xhuoying_liudaoban.jpg",
@@ -431,6 +431,8 @@ extension["新武将"]={
  "wms_backgroundmusic.mp3","wms_shixing_background.jpg","xwj_xwugeng_xuanfeng.jpg","xwj_xwugeng_baofeng1.mp3","xwj_xwugeng_baofeng2.mp3",
  "xwj_xwugeng_zhengkong1.mp3","xwj_xwugeng_zhengkong2.mp3","xwj_xwugeng_pili1.mp3","xwj_xwugeng_pili2.mp3","xwj_xwugeng_zhengnu1.mp3","xwj_xwugeng_zhengnu2.mp3",
  "xwj_xwugeng_tiankui.jpg","xwj_xwugeng_shenqu1.mp3","xwj_xwugeng_shenqu2.mp3","xwj_xwugeng_suiyue1.mp3","xwj_xwugeng_suiyue2.mp3",
+  "xwj_xxiaoyuan_mssyj.jpg", "xwj_xxiaoyuan_ylwg.jpg",
+ 
 ]
 
 };

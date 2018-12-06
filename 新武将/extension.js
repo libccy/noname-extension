52b3c62f30c36671ce049cbda6753d12abbb28c5
jianshi <33780347+Sukincen@users.noreply.github.com>
2018-12-06 20:31:51 +0800

    Update extension.js

--- 新武将/extension.js
+++ 新武将/extension.js
@@ -13027,5 +13027,5 @@ if(!lib.config.cards.contains('xwj_xus_equip')) lib.config.cards.remove('xwj_xus
     author:"★Sukincen★",
     diskURL:"",
     forumURL:"",
-    version:"2.47",
+    version:"2.48",
 },files:{"character":[],"card":[],"skill":[]}}})

9f0a2b9a90d086821d236338bdf21a168c8043b7
Sukincen <33780347+Sukincen@users.noreply.github.com>
2019-03-23 23:41:13 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -498,14 +498,15 @@ extension["鲍三娘"]={
 
 extension["金庸群侠传"]={
     author:"Sukincen",
-    intro:"<li>技能设计：大熊小猫 <li>编写代码：Sukincen <li>友情配音：觅阳 <li>更新：修复ai，新增武将张三丰，目前武将共4名 <li>更新日期：2019-03-22",
-    size:"5.5MB",
+    intro:"<li>技能设计：大熊小猫 <li>编写代码：Sukincen <li>友情配音：觅阳 <li>更新：修复BUG，补充配音，目前武将共4名 <li>更新日期：2019-03-23",
+    size:"6.5MB",
     netdisk:"",
-    version:1.5,
+    version:1.6,
     files:[
-        "extension.js","sdxl_anhun1.mp3","sdxl_anhun2.mp3","sdxl_simou1.mp3","sdxl_simou2.mp3",
-		"sdxl_xiaolongniv.jpg","sdxl_yangguo.jpg","sdxl_yingxia1.mp3","sdxl_yingxia2.mp3",
-		"tlbb_duanyanqing.jpg","tlbb_liuwang1.mp3","tlbb_liuwang2.mp3","tlbb_qiangcan1.mp3",
+        "extension.js","sdxl_anhun1.mp3","sdxl_anhun2.mp3","sdxl_hebi1.mp3","sdxl_hebi2.mp3",
+		"sdxl_luowang1.mp3","sdxl_luowang2.mp3","sdxl_muzong1.mp3","sdxl_muzong2.mp3","sdxl_simou1.mp3",
+		"sdxl_simou2.mp3","sdxl_xiaolongniv.jpg","sdxl_yangguo.jpg","sdxl_yingxia1.mp3",
+		"sdxl_yingxia2.mp3","tlbb_duanyanqing.jpg","tlbb_liuwang1.mp3","tlbb_liuwang2.mp3",
 		"tlbb_qiangcan2.mp3","tlbb_rangquan1.mp3","tlbb_rangquan2.mp3","yttl_chunyan1.mp3",
 		"yttl_chunyan2.mp3","yttl_taiji1.mp3","yttl_taoli1.mp3","yttl_taoli2.mp3","yttl_tiji2.mp3",
 		"yttl_zhangsanfeng.jpg"

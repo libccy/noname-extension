a9c7e64f0b6faa4b31d5cd076a881df1c2fef925
Sukincen <33780347+Sukincen@users.noreply.github.com>
2019-04-05 09:49:56 +0800

    Update extension.js

--- 群英会/extension.js
+++ 群英会/extension.js
@@ -14848,5 +14848,5 @@ if(!lib.config.cards.contains('xwj_xus_equip')) lib.config.cards.remove('xwj_xus
     author:"★Sukincen★",
     diskURL:"",
     forumURL:"",
-    version:"1.51",
+    version:"1.52",
 },files:{"character":[],"card":[],"skill":[]}}})

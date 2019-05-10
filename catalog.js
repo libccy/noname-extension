37f5f71648d896ce614bb8279715bb94aeb59e1d
Sukincen <33780347+Sukincen@users.noreply.github.com>
2019-05-10 14:15:40 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -339,10 +339,10 @@ extension["群英会"]={
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",
 	size:"27MB",
-    version:1.61,
+    version:1.62,
     files:
 
-["extension.js","wms_tian_background.jpg","wms_backgroundmusic.mp3","wms_background.jpg",
+["extension.js","wms_tian_background.jpg","wms_backgroundmusic.mp3","wms_background.jpg","wms_wxyd_background.jpg",
 		"wms_shixing_background.jpg","xwj_xhuoying_aiyuan1.mp3","xwj_xhuoying_aiyuan2.mp3",
 		"xwj_xhuoying_anqi1.mp3","xwj_xhuoying_anqi2.mp3","xwj_xhuoying_ansha1.mp3","xwj_xhuoying_ansha2.mp3",
 		"xwj_xhuoying_asima.jpg","xwj_xhuoying_baihao1.mp3","xwj_xhuoying_baihao2.mp3","xwj_xhuoying_baiji1.mp3",

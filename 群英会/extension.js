4b507ce427d627d01dff8b49c7063994746e3dc7
Sukincen <33780347+Sukincen@users.noreply.github.com>
2019-06-30 17:23:10 +0800

    Update extension.js

--- 群英会/extension.js
+++ 群英会/extension.js
@@ -14642,7 +14642,7 @@ skill:{
                 return -2;
             },
 					 },
-                    //order:4,
+                    order:4,
                     expose:0.4,
                 },
             },

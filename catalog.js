982526b6f96a62b2a911fd2a8262d09046480841
Sukincen <33780347+Sukincen@users.noreply.github.com>
2020-01-16 12:20:29 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -21,12 +21,12 @@ extension["官渡之战"]={
 
 
 extension["群英会"]={
-	intro:"<li>更新：新增并改进凉茶写的【兵乐特效】。当前版本为1.106  <li>本扩展原名为：新武将。包含部分<span class=bluetext>三国新将</span> <font color=#f00>火影忍者</font> <span class=greentext>秦时明月</span> <span class=browntext>武庚纪</span>、龙珠、战国、武侠等作品或类别的人物，技能新颖独特，强度略高但彼此平衡（平衡党止步）。有<font color=#f00>击杀特效</font> 可联机！素材为高清图片、高质配音。<li>欢迎加入无名杀扩展交流Q群：852740627<li>更新日期：2020-01-12",
+	intro:"<li>更新：注释掉【尸鬼封尽】的动画并为被封印角色加封印效果。当前版本为1.107  <li>本扩展原名为：新武将。包含部分<span class=bluetext>三国新将</span> <font color=#f00>火影忍者</font> <span class=greentext>秦时明月</span> <span class=browntext>武庚纪</span>、龙珠、战国、武侠等作品或类别的人物（没图片的解决方法：开启武将项相应分扩展栏的总开关，然后重启游戏），技能新颖独特，强度略高但彼此平衡（平衡党止步）。有<font color=#f00>击杀特效</font> 可联机！素材为高清图片、高质配音。<li>欢迎加入无名杀扩展交流Q群：852740627<li>更新日期：2020-01-16",
 	author:"小苏&&咫尺天涯",
 	netdisk:"https://pan.baidu.com/s/1hrR0p3u",
 	forum:"https://tieba.baidu.com/p/5288180800?share=9105&fr=share&see_lz=0&sfc=qqfriend&client_type=2&client_version=8.9.0.0&st=1510983198&unique=F72066C71DBA30E5FEAC1F522434D98E",
 	size:"90MB",
-    version:1.106,
+    version:1.107,
     files:
 
 ["extension.js","wms_background.jpg","wms_backgroundmusic.mp3","wms_shixing_background.jpg",
@@ -54,7 +54,7 @@ extension["群英会"]={
 		"xwj_xhuoying_dayemu.jpg","xwj_xhuoying_didala.jpg","xwj_xhuoying_dingchi.jpg","xwj_xhuoying_dou.jpg",
 		"xwj_xhuoying_duan.jpg","xwj_xhuoying_duanbi1.mp3","xwj_xhuoying_duanbi2.mp3","xwj_xhuoying_duanbi21.mp3",
 		"xwj_xhuoying_duanbi22.mp3","xwj_xhuoying_duizhang1.mp3","xwj_xhuoying_duizhang2.mp3",
-		"xwj_xhuoying_duoyang1.mp3","xwj_xhuoying_duoyang2.mp3","xwj_xhuoying_feiduan.jpg","bingletexiaole.png","bingletexiaobingliang.jpg",
+		"xwj_xhuoying_duoyang1.mp3","xwj_xhuoying_duoyang2.mp3","xwj_xhuoying_feiduan.jpg","bingletexiaole.png","bingletexiaobingliang.jpg","xhuoying_fengyin.png",
 		"xwj_xhuoying_feidun1.mp3","xwj_xhuoying_feidun2.mp3","xwj_xhuoying_feijian.jpg",
 		"xwj_xhuoying_fengdun1.mp3","xwj_xhuoying_fengdun2.mp3","xwj_xhuoying_fenglian1.mp3",
 		"xwj_xhuoying_fenglian2.mp3","xwj_xhuoying_fengwang1.mp3","xwj_xhuoying_fengwang2.mp3",

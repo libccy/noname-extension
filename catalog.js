71dc6d0536de6f10a6b62bff68ac67638902568a
shijian <2954700422@qq.com>
2023-10-05 01:55:25 +0800

    创建development分支

--- catalog.js
+++ catalog.js
@@ -1,9 +1,9 @@
-if (!localStorage.getItem('old-extension-tip')) {
-	alert('扩展仓库升级中，' +
-		'您在游戏内的扩展栏目获取扩展时可能会下载到四五年前的扩展导致游戏崩溃' +
-		'，升级完成前请谨慎下载');
-	localStorage.setItem('old-extension-tip', 'true');
-}
+// if (!localStorage.getItem('old-extension-tip')) {
+// 	alert('扩展仓库升级中，' +
+// 		'您在游戏内的扩展栏目获取扩展时可能会下载到四五年前的扩展导致游戏崩溃' +
+// 		'，升级完成前请谨慎下载');
+// 	localStorage.setItem('old-extension-tip', 'true');
+// }
 
 extension["官渡之战"] = {
 	date:"2022/01/11",

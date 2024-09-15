0bb1c3818418b7fa2aa5d0c7a9f9a5c02286163a
Sukincen <33780347+Sukincen@users.noreply.github.com>
2024-09-15 19:24:51 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -1,7 +1,7 @@
 if (!localStorage.getItem('old-extension-tip')) {
 	alert('扩展仓库升级中，' +
 		'您在游戏内的扩展栏目获取扩展时可能会下载到四五年前的扩展导致游戏崩溃' +
-		'，升级完成前请谨慎下载（《文武英杰》扩展仍在此持续更新）');
+		'，升级完成前请谨慎下载');
 	localStorage.setItem('old-extension-tip', 'true');
 }
 

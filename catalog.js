6cd83ef18aae760ae66f07d58490bf2fc76ee6a6
Sukincen <33780347+Sukincen@users.noreply.github.com>
2024-09-21 10:38:45 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -3,7 +3,7 @@ if (!localStorage.getItem('old-extension-tip')) {
 		'您在游戏内的扩展栏目获取扩展时可能会下载到四五年前的扩展导致游戏崩溃' +
 		'，升级完成前请谨慎下载');
 	localStorage.setItem('old-extension-tip', 'true');
-}
+};
 
 extension["官渡之战"] = {
 	date:"2022/01/11",

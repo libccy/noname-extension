f01bcfe3c3bfc8f2d96d83cb485ad04da0fdace8
Sukincen <33780347+Sukincen@users.noreply.github.com>
2025-10-10 10:41:50 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -12,7 +12,7 @@ extension["火影忍者"] = {
 	intro: "<li>时隔五年，从《群英会》扩展中独立出来的火影忍者扩展，将技能、代码全部翻新了一遍，有剧情模式和乱斗图鉴功能<li>修复BUG",
 	size: "60MB",
 	netdisk: "",
-	version: 2.3,
+	version: 2.4,
 	files: [
 		"LICENSE","huoying_yan.mp3","hyrz_zbfs.mp3","huoying_aiyuan1.mp3","huoying_aiyuan2.mp3","hyrz_close.mp3","hyrz_danchuang.mp3",
 		"huoying_anqi1.mp3","huoying_anqi2.mp3","huoying_ansha1.mp3","huoying_ansha2.mp3","huoying_asima.jpg","huoying_bai.jpg",
@@ -98,7 +98,7 @@ extension["文武英杰"] = {
 	author: "凉茶",
 	size: "54MB",
 	netdisk: "",
-	version: 5.2,
+	version: 5.3,
 	files: [
 	"LICENSE","newtujian.js","skin.js","update.js","wenwuyingjie.jpg","wenwuyingjiepicture.jpg","wwyj_ancha1.mp3","wwyj_close.mp3",
 		"wwyj_anshas.jpg","wwyj_anshas.mp3","wwyj_baozao1.mp3","wwyj_baozaorecover1.mp3","wwyj_bingmou.mp3","wwyj_bohe1.mp3",
@@ -161,7 +161,7 @@ extension["群英会"] = {
 	netdisk: "",
 	forum: "",
 	size: "43MB",
-	version: 2.2,
+	version: 2.3,
 	files:
 
 		["LICENSE","wugeng_poji1.mp3","wugeng_poji2.mp3","LICENSE","qinshi_feigong1.mp3","qinshi_feigong2.mp3","qinshi_genie.jpg","qinshi_hengjian1.mp3",

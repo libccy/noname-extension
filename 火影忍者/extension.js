d6e3576b7817a633a757fd641303ad2d8bef3699
Sukincen <33780347+Sukincen@users.noreply.github.com>
2025-11-03 09:19:40 +0800

    Add files via upload

--- 火影忍者/extension.js
+++ 火影忍者/extension.js
@@ -12667,7 +12667,7 @@ game.import("extension", function (lib, game, ui, get, ai, _status) {
             author: "小苏<li><div onclick=window.open('https://jq.qq.com/?_wv=1027&k=5qvkVxl')><span style=\"color: green;text-decoration: underline;font-style: oblique\">点击此处</span></div><span style=\"font-style: oblique\">申请加入QQ群（852740627）参与讨论。</span>",
             diskURL: "",
             forumURL: "",
-            version: "2.7",
+            version: "2.8",
         }, files: { "character": [], "card": [], "skill": [] }
     }
 })
